799669d4e4bc4655f4f8b5d2ea3d5e7304137be2
Ylan Segal <ylan@segal-family.com>
2019-07-17 15:22:43 -0700

    Add rspec specs for command line script, as proof of concept

---
+++ spec/jira_ticket_number_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe 'jira_ticket_number' do
+  TRUTH_TABLE = {
+    'CF-7847/tc/woc_line_item_fk_validation' => 'CF-7847',
+    'PSI-2534' => 'PSI-2534',
+    'ys/CF-8176_rework_request_sweeper' => 'CF-8176',
+    'ys/CF-7996_upgrade_ruby_2.6' => 'CF-7996'
+  }
+
+  TRUTH_TABLE.each do |input, expected_output|
+    it "it extracts #{expected_output} from #{input}" do
+      output = %x{jira_ticket_number #{input}}
+      expect(output).to eq("#{expected_output}\n")
+    end
+  end
+end
