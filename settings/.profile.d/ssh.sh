1f4f120521d55adab9792da02d45de5c7bbd1a8f
Ylan Segal <ylan@segal-family.com>
2019-10-30 09:29:00 -0700

    Wrap ssh by default with tmux, so I can use autocomplete

--- settings/.profile.d/ssh.sh
+++ settings/.profile.d/ssh.sh
@@ -4,11 +4,7 @@
 ssh() {
   # Set iTerm profile to Ssh
   echo -e "\033]50;SetProfile=Ssh\a"
-  command ssh "$@"
-  echo -e "\033]50;SetProfile=Default\a"
-}
-
-ssh_tmux() {
   # Atatch or create a a "main" tmux session, configured for iTerm
-  ssh -t $1 'tmux -CC new -A -s main'
+  command ssh -t "$@" 'tmux -CC new -A -s main'
+  echo -e "\033]50;SetProfile=Default\a"
 }
