272811a996b1286c7d847d27e7b0387b86d153a7
Ylan Segal <ylan@segal-family.com>
2019-11-01 12:44:52 -0700

    Remove utilities not needed anymore
    They were previuosly used for trigger_circle_build

--- spec/git-account_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe 'git account' do
-  it 'finds github account for current repo' do
-    expect(%x{ git account }).to eq(<<~UNIX)
-      ylansegal
-    UNIX
-  end
-end
--- spec/git-project_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe 'git account' do
-  it 'finds github project for current repo' do
-    expect(%x{ git project }).to eq(<<~UNIX)
-      dotfiles
-    UNIX
-  end
-end
