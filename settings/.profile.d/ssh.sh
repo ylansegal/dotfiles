3dc359016278d46c58ce62b6c1eebda8bfbd3f2a
Ylan Segal <ylan@segal-family.com>
2019-11-04 10:14:07 -0800

    Only start tmux automatically when connecting to procore servers

--- settings/.profile.d/ssh.sh
+++ settings/.profile.d/ssh.sh
@@ -4,7 +4,15 @@
 ssh() {
   # Set iTerm profile to Ssh
   echo -e "\033]50;SetProfile=Ssh\a"
-  # Atatch or create a a "main" tmux session, configured for iTerm
-  command ssh -t "$@" 'tmux -CC new -A -s main'
+
+  # Are we connecting to a procore server?
+
+  if [[ ${@: -1} =~ procore ]]; then
+    # Atatch or create a a "main" tmux session, configured for iTerm
+    set -x
+    command ssh -t "$@" 'tmux -CC new -A -s main'
+  else
+    command ssh "$@"
+  fi
   echo -e "\033]50;SetProfile=Default\a"
 }
