2b173f615a93aa3f28327295a3e8ea18ac4fcd56
Ylan Segal <ylan@segal-family.com>
2020-04-13 10:25:13 -0700

    Add alias useful for git spelunking

--- settings/.profile.d/aliases.sh
+++ settings/.profile.d/aliases.sh
@@ -5,3 +5,4 @@ alias tig="tig --show-signature"
 alias iso_date="date "+%Y-%m-%d""
 alias seeing_is_believing="seeing_is_believing --xmpfilter-style"
 alias fuzzy_search="sk --ansi -i -c 'rg --color=always --line-number \"{}\"' | cut -f1 -d:"
+alias inflate='ruby -r zlib -e "STDOUT.write Zlib::Inflate.inflate(STDIN.read)"'
