3b1c0aff599ea61f51b8e6e29761d9022ab013e3
Ylan Segal <ylan@segal-family.com>
2020-05-14 11:39:51 -0700

    Alias for stripping out last line feed

--- settings/.profile.d/aliases.sh
+++ settings/.profile.d/aliases.sh
@@ -6,3 +6,4 @@ alias iso_date="date "+%Y-%m-%d""
 alias seeing_is_believing="seeing_is_believing --xmpfilter-style"
 alias fuzzy_search="sk --ansi -i -c 'rg --color=always --line-number \"{}\"' | cut -f1 -d:"
 alias inflate='ruby -r zlib -e "STDOUT.write Zlib::Inflate.inflate(STDIN.read)"'
+alias strip="xargs echo -n"
