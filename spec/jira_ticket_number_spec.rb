d7357c55e14992fb13d4d9f36cb4230ba94653d1
Ylan Segal <ylan@segal-family.com>
2020-10-29 16:22:38 -0700

    Fix style violations

--- spec/jira_ticket_number_spec.rb
+++ spec/jira_ticket_number_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 
 RSpec.describe "jira_ticket_number" do
-  JIRA_TRUTH_TABLE = {
+  jira_truth_table = {
     "CF-7847/tc/woc_line_item_fk_validation" => "CF-7847",
     "PSI-2534" => "PSI-2534",
     "ys/CF-8176_rework_request_sweeper" => "CF-8176",
     "ys/CF-7996_upgrade_ruby_2.6" => "CF-7996"
   }.freeze
 
-  JIRA_TRUTH_TABLE.each do |input, expected_output|
+  jira_truth_table.each do |input, expected_output|
     it "it extracts #{expected_output} from #{input}" do
       output = `jira_ticket_number #{input}`
       expect(output).to eq("#{expected_output}\n")
