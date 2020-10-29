66bd68247bff774c93aa3a145415b7542bf1b388
Ylan Segal <ylan@segal-family.com>
2020-10-29 11:01:06 -0700

    Rename clip_title -> zt_title

--- spec/zt_title_spec.rb
+++ spec/zt_title_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 require "date"
 
-RSpec.describe "clip_title" do
+RSpec.describe "zt_title" do
   CLIP_TITLE_TRUTH_TABLE = {
     "Millions of tiny databases" => "Clips/#{Date.today}-MillionsOfTinyDatabases",
     "A successful Git branching model" => "Clips/#{Date.today}-ASuccessfulGitBranchingModel",
@@ -10,12 +10,12 @@ RSpec.describe "clip_title" do
 
   CLIP_TITLE_TRUTH_TABLE.each do |input, expected_output|
     it "generates a clip file title from command arguments (#{input})" do
-      output = `clip_title #{input}`
+      output = `zt_title #{input}`
       expect(output).to eq("#{expected_output}\n")
     end
 
     it "generates a clip file title from stdin (#{input})" do
-      output = `echo #{input} | clip_title`
+      output = `echo #{input} | zt_title`
       expect(output).to eq("#{expected_output}\n")
     end
   end
