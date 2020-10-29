a61ed29f3c2f0d7722038fa604725a0c567a372d
Ylan Segal <ylan@segal-family.com>
2020-10-29 16:22:19 -0700

    Change spec name, to correspond to command

--- spec/zk_title_spec.rb
+++ spec/zk_title_spec.rb
@@ -1,21 +1,21 @@
 require "spec_helper"
 require "date"
 
-RSpec.describe "zt_title" do
-  CLIP_TITLE_TRUTH_TABLE = {
+RSpec.describe "zk_title" do
+  zk_title_truth_table = {
     "Millions of tiny databases" => "#{Date.today}-MillionsOfTinyDatabases",
     "A successful Git branching model" => "#{Date.today}-ASuccessfulGitBranchingModel",
     "Build a Twitter with Rails, CableReady, and StimulusReflex" => "#{Date.today}-BuildATwitterWithRailsCableReadyAndStimulusReflex"
   }.freeze
 
-  CLIP_TITLE_TRUTH_TABLE.each do |input, expected_output|
+  zk_title_truth_table.each do |input, expected_output|
     it "generates a clip file title from command arguments (#{input})" do
-      output = `zt_title #{input}`
+      output = `zk_title #{input}`
       expect(output).to eq("#{expected_output}\n")
     end
 
     it "generates a clip file title from stdin (#{input})" do
-      output = `echo #{input} | zt_title`
+      output = `echo #{input} | zk_title`
       expect(output).to eq("#{expected_output}\n")
     end
   end
