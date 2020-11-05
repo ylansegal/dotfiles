58f8753a29f39c781f0c2438e5f987d23c7eac5a
Ylan Segal <ylan@segal-family.com>
2020-11-04 16:41:59 -0800

    Use fd for finding, instead of find
    Also show symbolic links

--- settings/.profile.d/cdp.sh
+++ settings/.profile.d/cdp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Changes to a project directory, found fuzzily
 cdp() {
-    dir=$(find -H ~/Development ~/Personal -maxdepth 1 -type d | sk)
+    dir=$(fd . --max-depth=1 --type d --type l ~/Development ~/Personal | sk)
     if [ ! -z "$dir" ]; then
       cd $dir || return
     fi
