e054f611d611943f625be24b2bb7c917ff3b59a7
Ylan Segal <ylan@segal-family.com>
2020-11-10 14:07:06 -0800

    Add tool to switch from file name to wiki-link

--- settings/.profile.d/aliases.sh
+++ settings/.profile.d/aliases.sh
@@ -7,3 +7,4 @@ alias seeing_is_believing="seeing_is_believing --xmpfilter-style"
 alias fuzzy_search="sk --ansi -i -c 'rg --color=always --line-number \"{}\"' | cut -f1 -d:"
 alias inflate='ruby -r zlib -e "STDOUT.write Zlib::Inflate.inflate(STDIN.read)"'
 alias strip="xargs echo -n"
+alias zk_file_to_link="sed -E 's/(.*)(\.md)/[[\1]]/'" # Converts a file name to wiki-style link
