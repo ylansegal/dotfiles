6ba04252842ee289bea517dd5d083c283e8ed884
Ylan Segal <ylan@segal-family.com>
2020-11-15 08:06:23 -0800

    Fix failing spec

--- spec/zk_file_to_link_spec.rb
+++ spec/zk_file_to_link_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
-require "date"
 
 RSpec.describe "zk_file_to_link" do
   it "generates a wiki link from a single file name" do
-    output = `echo "Clips/2020-05-10-SomeGreatTitle.md" | zk_file_to_link`
+    input = "Clips/2020-05-10-SomeGreatTitle.md"
+
+    output = `echo #{input} | zk_file_to_link`
     expect(output).to eq("[[Clips/2020-05-10-SomeGreatTitle]]\n")
   end
 
   it "generates wiki links from a list of file names" do
-    pending "Works in the console, but not in the spec. Don't know why"
     input = <<~INPUT
       Clips/2020-05-10-SomeGreatTitle.md
       Procore/Tickets/FINF-1345.md
@@ -21,6 +21,8 @@ RSpec.describe "zk_file_to_link" do
       [[Naked]]
     OUTPUT
 
-    expect(`echo #{input} | zk_file_to_link`).to eq(expected_output)
+    output = `echo "#{input.strip}" | zk_file_to_link`
+
+    expect(output).to eq(expected_output)
   end
 end
